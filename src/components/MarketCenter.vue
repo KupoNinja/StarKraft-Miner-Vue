ab81d4fd9656c92fb6a701310090970be307957f
JJ Garlitos <jj.garlitos@gmail.com>
2020-03-19 20:56:29 -0600

    Split out clickUpgrades and idleUpgrades

--- src/components/MarketCenter.vue
+++ src/components/MarketCenter.vue
@@ -3,7 +3,10 @@
     <h3>MarketCenter</h3>
     <hr class="text-light" />
     <h4>Click Upgrades:</h4>
-    <div v-for="upgrade in upgrades" :key="upgrade.name" col-sm-6>
+    <div v-for="upgrade in upgrades.clickUpgrades" :key="upgrade.name" col-sm-6>
+      <Upgrade :upgrade="upgrade" />
+    </div>
+    <div v-for="upgrade in upgrades.idleUpgrades" :key="upgrade.name" col-sm-6>
       <Upgrade :upgrade="upgrade" />
     </div>
   </div>
--- src/store/index.js
+++ src/store/index.js
@@ -8,21 +8,40 @@ export default new Vuex.Store({
     mineralCount: 0,
     idleCount: 0,
     totalCount: 0,
-    upgrades: [
-      {
-        name: "SCV",
-        price: 25,
-        quantity: 0,
-        multiplier: 1
-      },
-      {
-        name: "Probe",
-        price: 120,
-        quantity: 0,
-        multiplier: 20
-      }
-    ],
-    inventory: []
+    upgrades: {
+      clickUpgrades: [
+        {
+          name: "SCV",
+          price: 25,
+          quantity: 0,
+          multiplier: 1
+        },
+        {
+          name: "Probe",
+          price: 120,
+          quantity: 0,
+          multiplier: 20
+        }
+      ],
+      idleUpgrades: [
+        {
+          name: "Reaver",
+          price: 450,
+          quantity: 0,
+          multiplier: 50
+        },
+        {
+          name: "Ultralisk",
+          price: 1000,
+          quantity: 0,
+          multiplier: 100
+        }
+      ]
+    },
+    inventory: {
+      clickUpgrades: [],
+      idleUpgrades: []
+    }
   },
   mutations: {},
   actions: {
